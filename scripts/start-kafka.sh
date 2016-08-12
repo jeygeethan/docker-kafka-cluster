6a3d8a4546a7441dc40de6a1fbc45a3c50eb938f
Jey <geetan123321@gmail.com>
2016-08-12 23:40:46 +0530

    enable topic deletion

--- scripts/start-kafka.sh
+++ scripts/start-kafka.sh
@@ -43,5 +43,11 @@ if [ ! -z "$AUTO_CREATE_TOPICS" ]; then
     echo "auto.create.topics.enable=$AUTO_CREATE_TOPICS" >> $KAFKA_HOME/config/server.properties
 fi
 
+# Configure the default number of log partitions per topic
+if [ ! -z "$DELETE_TOPIC_ENABLE" ]; then
+    echo "delete.topic.enable: $DELETE_TOPIC_ENABLE"
+    sed -r -i "s/(delete.topic.enable)=(.*)/\1=$DELETE_TOPIC_ENABLE/g" $KAFKA_HOME/config/server.properties
+fi
+
 # Run Kafka
 $KAFKA_HOME/bin/kafka-server-start.sh $KAFKA_HOME/config/server.properties
